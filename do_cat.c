ad6397c952e87c7f8f4664c5c2c4cc43b0078502
kassy11 <kotaro11kassy@gmail.com>
2020-07-09 23:59:37 +0900

    cat修正

--- do_cat.c
+++ do_cat.c
@@ -8,7 +8,7 @@
 #define BUFFER_SIZE 2048
 
 static void do_cat(const char* path);
-static void die(const char* s);
+static void errormsg(const char* s);
 
 int main(int argc, char* argv[]){
   int i;
@@ -30,19 +30,19 @@ static void do_cat(const char *path){
 
   fd = open(path, O_RDONLY);
   if(fd < 0){
-    die(path);
+    errormsg(path);
   }
   for(;;){
     n = read(fd, buf, sizeof buf );
-    if(n<0) die(path);
+    if(n<0) errormsg(path);
     if(n == 0) break;
-    if(write(STDOUT_FILENO, buf, n) < 0) die(path);
+    if(write(STDOUT_FILENO, buf, n) < 0) errormsg(path);
   }
 
-  if(close(fd) < 0) die(path);
+  if(close(fd) < 0) errormsg(path);
 }
 
-void die(const char *s){
+void errormsg(const char *s){
   perror(s);
   exit(1);
 }
