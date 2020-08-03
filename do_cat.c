bc052c195d4625c564a70a8789c2b3c6c11e45de
kassy11 <kotaro11kassy@gmail.com>
2020-08-03 17:14:54 +0900

    do_cat改良版

--- do_cat.c
+++ do_cat.c
@@ -17,14 +17,18 @@ int main(int argc, char* argv[]){
   int i;
   char filename[DEFAULT_FILENAME_LEN][256];
 
-      for (i=0; i<argc; i++){
-          strcpy(filename[i], argv[i]);
+  if(argc==1){
+      char input[20];
+      char *ptr;
+      scanf("%s", input);
+      ptr = strtok(input, " ");
+      do_cat(ptr);
+  }else{
+      for(i=1; i<argc; i++){
+          do_cat(argv[i]);
       }
-
-
-  for(i=0; i<argc; i++){
-    do_cat(filename[i]);
   }
+  exit(0);
 }
 
 
@@ -33,14 +37,17 @@ static void do_cat(const char *path){
   unsigned char buf[BUFFER_SIZE];
   int n;
 
+  // 読み込み専用でopenする
   fd = open(path, O_RDONLY);
-  if(fd < 0){
-    errormsg(path);
-  }
+  if(fd < 0)errormsg(path);
+
   for(;;){
+      // ストリームfdからbufにバイト列をsizeof bufだけ読み込む
     n = read(fd, buf, sizeof buf );
     if(n<0) errormsg(path);
+    // read()は全て読み込んだ時0を返す
     if(n == 0) break;
+    //
     if(write(STDOUT_FILENO, buf, n) < 0) errormsg(path);
   }
 
@@ -48,6 +55,7 @@ static void do_cat(const char *path){
 }
 
 void errormsg(const char *s){
+    // perrorはerrnoに合ったエラー分を出力する
   perror(s);
   exit(1);
 }
